b7f2b8f5675378b8343053891d108bd907f81a47
devy999 <82936187+devy999@users.noreply.github.com>
2021-05-15 11:20:09 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_admin import Admin
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
 
-@app.route('/')
-def home():
-    return "Hello World."
-@app.route('/welcome')
-def welcome():
-    return render_template(welcome.html)
 
-if __name__=="__main__":
-    app.run(debug=True)
+from app import routes, models
+from app.models import (....)
+
+
