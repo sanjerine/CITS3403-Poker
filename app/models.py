7d2bf18aa309f473248f88f20e919f1e6951635e
devy999 <82936187+devy999@users.noreply.github.com>
2021-05-15 11:34:12 +0800

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,7 @@
+#database
+
+from app import db, login
+
+
+#create database using mixin from flask_login or otherwise
+#one for users, one for the poker hands (4 suits, 13 cards), one for outcome? dunno
