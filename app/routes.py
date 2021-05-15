1305b0a76d9f81ff6b2199975961e3bf6c34bc0d
devy999 <82936187+devy999@users.noreply.github.com>
2021-05-15 20:41:57 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -67,7 +67,7 @@ def stats():
 @app.route('/register')
 def register():
   if current_user.is_authenticated:
-    return redirect(url_for('general'))
+    return redirect(url_for('index'))
   form = RegistrationForm()
   if form.validate_on_submit():
     user = User(username=form.username.data, email=form.data)
