73b0d9ddb3813cbd5538d140f3406a2abbf2e135
incman7 <82936172+incman7@users.noreply.github.com>
2021-05-15 17:11:15 +0800

    Update routes.py
    added basic routes to all pages

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+
+  
 from flask import Flask, render_template
 from app import app,db
 from flask_login import (username...etc)
@@ -13,3 +15,36 @@ from app.forms import #all the classes of registration whatever
 
 @app.route('/')
 #all the routes that we want to include underneath here
+@app.route('/home')
+def home():
+    return render_template('home.html', title = 'Home') 
+
+@app.route('learn')
+def learn():
+    return render_template('learn.html', title = 'Learn') 
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title = 'Login') 
+
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html', title = 'Quiz') 
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html', title = 'Feedback') 
+
+@app.route('/stats')
+def stats():
+    return render_template('stats.html', title = 'Statistics') 
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('index'))
+    
+@app.route('/register')
+def register():
+    return render_template('register.html', title = 'register') 
+
+
