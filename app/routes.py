638581b1f7ad47553fdd1e950f6796740ff826e4
incman7 <82936172+incman7@users.noreply.github.com>
2021-05-15 18:51:14 +0800

    login registration

--- app/routes.py
+++ app/routes.py
@@ -26,7 +26,17 @@ def learn():
 
 @app.route('/login')
 def login():
-    return render_template('login.html', title = 'Login') 
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/quiz')
 def quiz():
