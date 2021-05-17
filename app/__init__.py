b236b8db0092084fca4ce9c9cc4901c68822f375
devy999 <82936187+devy999@users.noreply.github.com>
2021-05-17 11:47:40 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,9 +10,20 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+login.login_view = 'login'
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+admin = Admin(app, name='webadmin', template_mode='bootstrap3')
 
+from flask_admin.contrib.sqla import ModelView
 
 from app import routes, models
-from app.models import (....)
+from app.models import User, Question, Answer, Results, Feedback, Progress
+
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Question, db.session))
+admin.add_view(ModelView(Answer, db.session))
+admin.add_view(ModelView(Results, db.session))
+admin.add_view(ModelView(Feedback, db.session))
+admin.add_view(ModelView(Progress, db.session))
 
 
