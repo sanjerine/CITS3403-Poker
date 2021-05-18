bfbedbf3720deeea30ecc9774a5f2b3317ac1124
devy999 <82936187+devy999@users.noreply.github.com>
2021-05-18 17:41:17 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -77,13 +77,13 @@ class Results(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     userid = db.Column(db.Integer, db.ForeignKey('user.id'))
     result = db.Column(db.Integer)
-    highscore = db.Column(db.Integer)
+
     
 class TutorialResults(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     userid = db.Column(db.Integer, db.ForeignKey('user.id'))
     result = db.Column(db.Integer)
-
+    highscore = db.Column(db.Integer)
 
 class Progress(db.Model):
     id = db.Column(db.Integer, primary_key = True)
