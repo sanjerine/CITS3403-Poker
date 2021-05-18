b0431cc47896607b2dc064a889ca0daf27e87258
incman7 <82936172+incman7@users.noreply.github.com>
2021-05-18 13:43:18 +0800

    more debugging

--- app/models.py
+++ app/models.py
@@ -10,13 +10,13 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     
     feedback = db.relationship('Feedback', backref='userfeedback', lazy ='dynamic')
     userresult = db.relationship('Results', backref='user', lazy = 'dynamic')
     
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+    #def __repr__(self):
+        #return '<User {}>'.format(self.username)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
