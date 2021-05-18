849bb6c5a87a2206c2c1b2703e7250fcdf60a13e
incman7 <82936172+incman7@users.noreply.github.com>
2021-05-18 13:43:45 +0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,19 +3,19 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
 
-class RegistrationForm(FlaskForm):
-    username=StringField('Username',validators=[DataRequired()])
-    email=StringField('Email',validators=[DataRequired()])
-    password=PasswordField('Password',validators=[DataRequired()])
-    repeatpassword=PasswordField('Repeat Password',validators=[DataRequired()],EqualTo('password'))
-    submit=SubmitField('Submit')
-
 class LoginForm(FlaskForm):
     username=StringField('Username',validators=[DataRequired()])
     password=PasswordField('Password',validators=[DataRequired()])
     remember_me=BooleanField('Remember Me')
     submit=SubmitField('Sign In')
-    
+
+class RegistrationForm(FlaskForm):
+    username=StringField('Username',validators=[DataRequired()])
+    email=StringField('Email',validators=[DataRequired()])
+    password=PasswordField('Password',validators=[DataRequired()])
+    repeatpassword=PasswordField('Repeat Password',validators=[DataRequired(),EqualTo('password')])
+    submit=SubmitField('Submit')
+
     def validate_username(self, username):
         user=User.query.filter_by(username=username.data).first()
         if user is not None:
@@ -26,3 +26,4 @@ class LoginForm(FlaskForm):
         if user is not None:
             raise ValidationError('Email has already been used, please use another')
     
+
